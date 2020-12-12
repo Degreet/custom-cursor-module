038ef7c57f5ee47779ed32ba1af5eed0532685c2
Degreet <degreetpro@gmail.com>
2020-12-12 20:04:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+setCursor("https://icon-library.com/images/cursor-icon-png/cursor-icon-png-7.jpg", document.body, {
+  size: "32px"
+})
---
+++ src/custom-cursor-module.js
@@ -0,0 +1,23 @@
+const { body } = document
+function setCursor(pathToImage, el, params={}) {
+  const cursor = document.createElement("div")
+  cursor.style.width = cursor.style.height = params?.size || "64px"
+  cursor.style.background = `url(${pathToImage}) no-repeat center top / cover`
+  cursor.style.position = "absolute"
+  
+  if (!params?.noUseOverflow) el.style.overflow = "hidden"
+  el.style.cursor = "none"
+  el.append(cursor)
+
+  addEventListener("mousemove", e => {
+    const x = e.pageX = e.clientX +
+      (el && el.scrollLeft || body && body.scrollLeft || 0) -
+      (el && el.clientLeft || body && body.clientLeft || 0)
+    const y = e.pageY = e.clientY +
+      (el && el.scrollTop || body && body.scrollTop || 0) -
+      (el && el.clientTop || body && body.clientTop || 0)
+
+    cursor.style.top = `${y}px`
+    cursor.style.left = `${x}px`
+  })
+}
